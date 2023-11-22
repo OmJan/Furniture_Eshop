cdb7a39d03c4a3abf09a78b79af8fa5b33cfee08
Om Prakash Jangid <opjan9649@gmail.com>
2023-11-23 01:06:24 +0530

    NewAdded

--- furnitureapp/models/service.py
+++ furnitureapp/models/service.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Service(models.Model):
     service_name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='upload/services/', blank=True, null=True)
 
     @staticmethod
     def get_all_sevices():
